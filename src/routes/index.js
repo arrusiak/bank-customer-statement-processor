ffd337b7c2d97490b7e04206dca3d8e73950316a
arrusiak <arusminasyan909@gmail.com>
2023-12-11 12:20:10 +0400

    structure customization

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
 import express from 'express';
 
+import CustomerStatementRouter from './customer-statement';
+
 const router = express.Router();
 
-import CustomerStatementController from '../controllers/customer-statement.controller.js';
+router.get('/', (req, res) => {
+    res.send('Hello');
+});
 
-// Require the upload middleware
-const upload = require('../middleware/ upload');
+router.use('/customer-statement', CustomerStatementRouter);
 
-
-router.post('/',  upload.single('file'), CustomerStatementController.uploadFile)
-
-export default router;
+module.exports = router;
